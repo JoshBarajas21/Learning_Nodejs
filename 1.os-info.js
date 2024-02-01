db1fc4a6152c1a6cac6e8a412b5ad4f7a5390ac8
JoshBarajas21 <l19251092@gamadero.tecnm.mx>
2024-02-01 14:18:09 -0600

    NodeJs Modules: os

---
+++ 1.os-info.js
@@ -0,0 +1,12 @@
+const os = require('node:os');
+
+console.log('Información del Sistema Operativo');
+console.log('------------------');
+
+console.log('Nombre del Sistema Operativo', os.platform());
+console.log('Versión del Sistema Operativo', os.release());
+console.log('Arquitectura', os.arch());
+console.log('CPUs', os.cpus()); // <--- vamos a poder escalar procesos en Node
+console.log('Memoria Libre', os.freemem() / 1024 / 1024);
+console.log('Memoria Total', os.totalmem() / 1024 / 1024);
+console.log('uptime', os.uptime() / 60 / 60);
---
+++ 1.os-info.mjs
@@ -0,0 +1,20 @@
+import {
+  platform,
+  release,
+  arch,
+  cpus,
+  freemem,
+  totalmem,
+  uptime,
+} from 'node:os';
+
+console.log('Información del Sistema Operativo');
+console.log('------------------');
+
+console.log('Nombre del Sistema Operativo', platform());
+console.log('Versión del Sistema Operativo', release());
+console.log('Arquitectura', arch());
+console.log('CPUs', cpus()); // <--- vamos a poder escalar procesos en Node
+console.log('Memoria Libre', freemem() / 1024 / 1024);
+console.log('Memoria Total', totalmem() / 1024 / 1024);
+console.log('uptime', uptime() / 60 / 60);
