1417d85de64151315bbb229b2dd338436eaa96f3
JoshBarajas21 <l19251092@gamadero.tecnm.mx>
2024-02-01 14:22:28 -0600

    NodeJs Modules process

---
+++ 7.process.js
@@ -0,0 +1,17 @@
+// argumentos de entrada
+// console.log(process.argv);
+
+// Controlar el proceso y su salida
+// process.exit(0)
+// process.exit(1) --> se cierra el proceso
+
+// Podemos controlar eventos del proceso
+// process.on('exit', () =>{
+//     // limpiar la pantalla
+// })
+
+// current workin directory
+console.log(process.cwd());
+
+// plataform
+console.log(process.env.PEPITO);
