c1db973fbfbfa837f5f9959d5c9cf50afa286f29
JoshBarajas21 <l19251092@gamadero.tecnm.mx>
2024-02-01 14:19:31 -0600

    NodeJs Modules readFile: Async and Sync forms

---
+++ 3.fs-readFileSync.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+
+console.log('Leyendo el primer archivo...');
+const text = fs.readFileSync('./archivo.txt', 'utf-8'); // forma asíncrona de leer archivos
+
+console.log(text);
+
+console.log('Haciendo cosas mientras lee el archivo ...');
+
+console.log('Leyendo el segundo archivo ...');
+const secondText = fs.readFileSync('./archivo2.txt', 'utf-8'); // forma asíncrona de leer archivos
+
+console.log(secondText);
